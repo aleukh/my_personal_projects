97410e483f7772885cf73db00aa35d3f2aec98fc
aleukh <aloysiusyap1999@gmail.com>
2023-02-24 03:15:16 +0800

    final working copy i guess

--- python/image-links-changer.py
+++ python/image-links-changer.py
@@ -2,15 +2,13 @@ import sys
 import os
 
 filename = sys.argv[1]
-#user_select = sys.argv[2]
-#f = open(filename, "r+")
-#print(f.read())
 
 with open(filename, "r+") as file:
 	data = file.readlines()
+	
+
 
 dir_path = os.path.realpath(filename)
-#print(dir_path)
 split_dir_path = dir_path.split("\\")
 print(split_dir_path)
 fullpath = ""
@@ -21,20 +19,23 @@ for i in range(3, len(split_dir_path) - 1, 1):
 fullpath = "".join([fullpath, "/images/"])
 print(fullpath)	
 
-for lines in data:
-	if lines.startswith("![[") == True:
-		testlines = lines.replace(" ", "%20")
-		testlines2 = testlines
-		testlines2 = testlines2.replace("![[", "![Image](")
-		testlines3 = testlines2
-		testlines3 = testlines3.replace("]]", ")")
-		testlines4 = testlines3.split("(")
-		testlines5 = ""
-		testlines5 = fullpath + testlines4[1]
-		full = ""
-		full = testlines4[0] + "(" + testlines5
-		#print(testlines4[1])
-		print(full)
+
+for i in range(0, len(data), 1):
+	if data[i].startswith("![[") == True:
+		tmp = data[i].replace(" ", "%20")
+		tmp = tmp.replace("![[", "![Image](")
+		tmp = tmp.replace("]]", ")")
+		tmpSplit = tmp.split("(")
+		fullPathTemp = ""
+		fullPathTemp = tmpSplit[0] + "(" + fullpath + tmpSplit[1]
+		data[i] = fullPathTemp
+
+with open(filename, "w") as fileOpen:
+	fileOpen.writelines(data)
+
+
+
+
 
 file.close()
 
